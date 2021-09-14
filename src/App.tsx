60b7609a0cd0a38de2cfb2bb5a97aa5256ce54ab
Seanmclem <seanmclements@gmail.com>
2021-09-13 23:05:51 -0400

    pink cube moves around

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import {
   ReactThreeFiber,
   useFrame,
   useThree,
+  Vector3,
 } from "@react-three/fiber";
 import { Stars } from "@react-three/drei";
-import { useRef } from "react";
-import { useBox, usePlane } from "@react-three/cannon";
+import { useRef, useState } from "react";
+// import { useBox, usePlane } from "@react-three/cannon";
 
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { DoubleSide } from "three";
 
 declare global {
   namespace JSX {
@@ -24,24 +26,49 @@ declare global {
 
 extend({ OrbitControls });
 
-const Box = () => {
+const Box = ({ position }: { position: any }) => {
+  const size = 2;
+  console.log({ position });
+
   return (
-    <mesh>
-      <boxGeometry args={[2, 2, 2]} />
+    <mesh position={[position.x as number, 1.0001, position.z]}>
+      <boxGeometry args={[size, size, size]} />
       <meshLambertMaterial color={"hotpink"} />
     </mesh>
   );
 };
 
-const Plane = () => {
-  const [ref] = usePlane(() => ({
-    rotation: [-Math.PI / 2, 0, 0],
-  }));
+const Plane = ({
+  setPosition,
+}: {
+  setPosition: React.Dispatch<React.SetStateAction<ReactThreeFiber.Vector3>>;
+}) => {
+  // const [ref] = usePlane(() => ({
+  //   rotation: [-Math.PI / 2, 0, 0],
+  // }));
+
+  const handleClick = (event: any) => {
+    const points: Vector3 = event.intersections[0].point;
+    console.log({ points });
+
+    setPosition(points);
+    debugger;
+  };
+
   return (
-    <mesh ref={ref} position={[0, 0, 0]} rotation={[-Math.PI / 2, 0, 0]}>
+    <mesh
+      position={[0, 0, 0]}
+      rotation={[Math.PI / 2, 0, 0]}
+      scale={[1, 1, 1]}
+      onClick={handleClick}
+    >
       <planeBufferGeometry args={[100, 100]} />
-      <meshLambertMaterial attach="material" color="lightblue" />
+      <meshBasicMaterial color="green" side={DoubleSide} />
     </mesh>
+    // <mesh ref={ref} position={[0, 0, 0]} rotation={[-Math.PI / 2, 0, 0]}>
+    //   <planeBufferGeometry args={[100, 100]} />
+    //   <meshLambertMaterial attach="material" color="lightblue" />
+    // </mesh>
   );
 };
 
@@ -64,7 +91,7 @@ const CameraControls = () => {
     <orbitControls
       ref={controls}
       args={[camera, domElement]}
-      enableZoom={false}
+      enableZoom={true}
       maxAzimuthAngle={Math.PI / 4}
       maxPolarAngle={Math.PI}
       minAzimuthAngle={-Math.PI / 4}
@@ -74,6 +101,11 @@ const CameraControls = () => {
 };
 
 export const App = () => {
+  const [position, setPosition] = useState<Vector3>({
+    x: 0,
+    y: 0,
+    z: 0,
+  } as Vector3);
   return (
     <div className="canvas-container">
       <Canvas style={{ height: window.innerHeight, width: window.innerWidth }}>
@@ -81,7 +113,8 @@ export const App = () => {
         <Stars />
         <ambientLight intensity={0.5} />
         <directionalLight position={[10, 15, 10]} color={"red"} />
-        <Box />
+        <Box position={position} />
+        <Plane setPosition={setPosition} />
       </Canvas>
     </div>
   );
