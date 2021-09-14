0e33956a6a29c62fbde0f55f3e2dc846115608fd
Seanmclem <seanmclements@gmail.com>
2021-09-13 21:33:24 -0400

    working cube idk

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,86 @@
-import { Canvas, extend } from "@react-three/fiber";
-// import { OrbitControls } from "@react-three/drei";
-// import { OrbitControls, TransformControls } from 'three-stdlib'
-// import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-// import { OrbitControls } from 'three-stdlib/controls/OrbitControls'
-// import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import {
+  Canvas,
+  extend,
+  ReactThreeFiber,
+  useFrame,
+  useThree,
+} from "@react-three/fiber";
+import { Stars } from "@react-three/drei";
+import { useRef } from "react";
+import { useBox, usePlane } from "@react-three/cannon";
 
-// extend({ OrbitControls })
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      orbitControls: ReactThreeFiber.Object3DNode<
+        OrbitControls,
+        typeof OrbitControls
+      >;
+    }
+  }
+}
+
+extend({ OrbitControls });
 
 const Box = () => {
   return (
     <mesh>
       <boxGeometry args={[2, 2, 2]} />
-      <meshStandardMaterial />
+      <meshLambertMaterial color={"hotpink"} />
     </mesh>
+  );
+};
+
+const Plane = () => {
+  const [ref] = usePlane(() => ({
+    rotation: [-Math.PI / 2, 0, 0],
+  }));
+  return (
+    <mesh ref={ref} position={[0, 0, 0]} rotation={[-Math.PI / 2, 0, 0]}>
+      <planeBufferGeometry args={[100, 100]} />
+      <meshLambertMaterial attach="material" color="lightblue" />
+    </mesh>
+  );
+};
+
+const CameraControls = () => {
+  // Get a reference to the Three.js Camera, and the canvas html element.
+  // We need these to setup the OrbitControls class.
+  // https://threejs.org/docs/#examples/en/controls/OrbitControls
+
+  const {
+    camera,
+    gl: { domElement },
+  } = useThree();
+
+  // Ref to the controls, so that we can update them on every frame using useFrame
+  const controls: any = useRef();
+  useFrame(
+    (state) => controls?.current?.update() && controls?.current?.update()
+  );
+  return (
+    <orbitControls
+      ref={controls}
+      args={[camera, domElement]}
+      enableZoom={false}
+      maxAzimuthAngle={Math.PI / 4}
+      maxPolarAngle={Math.PI}
+      minAzimuthAngle={-Math.PI / 4}
+      minPolarAngle={0}
+    />
   );
 };
 
 export const App = () => {
   return (
     <div className="canvas-container">
-      <Canvas>
-        {/* <orbitControls /> */}
-        {/* <OrbitControls enableDamping /> */}
-        <ambientLight intesnsity={0.1} />
-        <directionalLight position={[0, 0, 5]} color={"red"} />
+      <Canvas style={{ height: window.innerHeight, width: window.innerWidth }}>
+        <CameraControls />
+        <Stars />
+        <ambientLight intensity={0.5} />
+        <directionalLight position={[10, 15, 10]} color={"red"} />
         <Box />
       </Canvas>
     </div>
