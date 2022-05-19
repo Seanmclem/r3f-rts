6cb1e12a3a2ba1bcb163cc8379fc083ad9db9b7a
Seanmclem <6831540+Seanmclem@users.noreply.github.com>
2022-05-19 19:21:08 -0400

    got better with cartesian coordinate style

--- src/components/grid/grid-service.ts
+++ src/components/grid/grid-service.ts
@@ -39,41 +39,42 @@ export const prepGridData = ({
 
   // ^ box-stubs?, kill w/fire
 
-  const filledOne = [1, 4]; /// TESTING
+  const filledOne = [2, 1]; /// TESTING ... 2,0 -> 2,3
   // NEED like a point-A and point-B to test a*
   // [0, 4] -> [3, 4], or visa versa
 
-  boxStubs.forEach((_columnNum, idX) => {
-    aStarArray[idX] = [];
-    gridData[idX] = [];
-    //if (idX < 4) {
-    boxStubs.forEach((_boxNumberByZero, idY) => {
-      aStarArray[idX].push(
-        JSON.stringify([idX, idY]) === JSON.stringify(filledOne) ? 0 : 1
+  boxStubs.forEach((_columnNum, idY) => {
+    // Y is first because row-after-row is vertically?
+    aStarArray[idY] = [];
+    gridData[idY] = [];
+    //if (idY < 4) {
+    boxStubs.forEach((_boxNumberByZero, idX) => {
+      aStarArray[idY].push(
+        JSON.stringify([idY, idX]) === JSON.stringify(filledOne) ? 0 : 1
       ); //(`a-${idX}-${idY}`);
       // should be 0 or 1's.
 
       /** covers the length of the board ... 20, for 5 by 100 */
       //   const cubesPlaneLength = planeSize / cubeSize;
 
-      const cubeNumber = idY + 1;
+      const rowNumber = idX + 1;
 
       const oneMoreCubePosition = cubeSize * 2;
 
-      const positionModifier = oneMoreCubePosition * cubeNumber - cubeSize;
-      const columnModifier = idX * cubeSize;
+      const positionModifier = oneMoreCubePosition * rowNumber - cubeSize;
+      const columnModifier = idY * cubeSize;
       const topPlane = planeSize / 2; // TOP of plane?
       const cubeTop = cubeSize / 2; // adjust coordinate from middle, to top, of cube
 
       // TODO:  use imports of `grid-types` format eventually here
 
       //if (idY < 4) {
-      gridData[idX].push({
-        // x should get bigger as it moves right?
+      gridData[idY].push({
+        // built row by row, horizontals are populated throughout
         xPosition: -topPlane + positionModifier / 2,
         // can swap Z/Y for ratate of thing order generation
         yPosition: cubeTop, // cube, bottom
-        zPosition: -topPlane + cubeTop + columnModifier,
+        zPosition: topPlane - (cubeTop + columnModifier),
         filled: !!(JSON.stringify([idX, idY]) === JSON.stringify(filledOne)),
       });
       //}
@@ -86,11 +87,11 @@ export const prepGridData = ({
   // [0, 4] -> [3, 4], or visa versa
 
   generatePath({
-    theGraph: aStarArray,
-    startX: 0,
-    startY: 4,
-    endX: 3,
-    endY: 4,
+    theGraph: aStarArray, // still fudging works
+    startX: 2,
+    startY: 0,
+    endX: 2,
+    endY: 3,
   });
 
   return { gridData, aStarArray };
